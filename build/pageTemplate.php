f463f000115fc737140c322ef5b4b52452ed43fe
nakzz <naqibprio10@gmail.com>
2018-04-30 03:56:31 -0500

    page template added

---
+++ build/pageTemplate.php
@@ -0,0 +1,187 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+
+
+	<!-- TITLE -->
+  <?php // TODO: Change tittle ?>
+	<title>Dairy Management</title>
+
+	<!-- DESCRIPTION -->
+
+	<meta name="description" content="This site is designed to support dairy farming
+	decision-making focusing on model-based scientific research.
+	The ultimate goal is to provide user-friendly computerized
+	decision support tools to help dairy farmers improve their
+	economic performance along with environmental  stewardship.">
+
+	<?php // TODO: change keywords ?>
+	<meta name="keywords"  content="Event, Coming Soon, Concert, Conference,
+	Counter, Exhibition, Festival, Landing Page, Music, One Page, Registration
+	Form, Seminar, Subscription, Mailchimp" />
+	<meta name="author" content="Ajmain Naqib" />
+
+	<?php
+	include 'includes/header.php';
+	?>
+
+</head>
+
+<body id="top" data-spy="scroll" data-target=".header" data-offset="80">
+
+	<!--PRELOADER
+	<div class="preloader">
+	<div class="status"></div>
+</div>
+<!--/PRELOADER-->
+
+<!--HEADER-->
+
+<?php
+include 'includes/nav-bar.php';
+?>
+
+<!--/HEADER-->
+<?php // TODO: add animation and transition ?>
+
+<!--SPEAKERS-->
+<section class="titleSection" >
+	<div class="container">
+		<div class="row">
+			<div class="wow animated fadeInUp" data-wow-duration="1s" data-wow-delay="1s">
+				<h1 class="arrow">First Title</h1><hr>
+			</div>
+		</div>
+    	</div>
+    </section>
+
+    <div style="background-color: #fff;"  class="container team">
+      <div style="color:black;" class="row">
+        <div class="col-sm-12" style="margin-top: 70px;">
+            <div class="col-sm-12">
+                <h2>Sample Text</h2>
+                <ul style="text-align:left; font-size:1.3em;" >
+                    <li>
+                      <p>Sameple list</p>
+                    </li>
+
+                    <li>
+                        2013. Wisconsin Association of County Agricultural Agents.<br/>
+                        Second Mile Award (<a href="./images/awards/PlaqueSecondMile.png" target="_blank">Plaque</a>) (<a href="./awards/CitationSecondMile.pdf" target="_blank">Citation</a>)
+                    </li>
+                    <li>
+                        2012. University of Wisconsin-Madison College of Agricultural and Life Sciences.<br/>
+                        Pound Extension Award (<a href="./awards/Pound_Letter.pdf" target="_blank">Letter</a>) (<a href="./images/awards/Pound_Plaque.png" target="_blank">Plaque</a>) (<a href="./awards/Pound_Words.pdf" target="_blank">Words</a>)
+                    </li>
+                    <li>
+                        2012. University of Florida School of Natural Resources and Environment.<br/>
+                        Distinguished Achievement Award (<a href="./images/awards/Distinguished_Plaque.png" target="_blank">Plaque</a>) (<a href="./awards/Distinguished_Words.pdf" target="_blank">Words</a>)
+                    </li>
+                    <li>
+                        2011. American Dairy Science Association.<br/>
+                        Foundation Scholar Award in Dairy Production (<a href="./awards/Foundation_Letter.pdf" target="_blank">Letter</a>) (<a href="./images/awards/Foundation_Plaque.png" target="_blank">Plaque</a>) (<a href="./awards/Foundation_Citation.pdf" target="_blank">Citations</a>)
+                    </li>
+                    <li>
+                        2011. University of Wisconsin-Madison College of Agricultural and Life Sciences.<br>
+                        Alfred Toepfer Faculty Fellow Award. (<a href="./awards/Toepfer_Letter.pdf" target="_blank">Letter</a>)
+                    </li>
+                </ul>
+            </div>
+
+            <div class="col-sm-12 paddingtopsmall">
+                <h2>Previous Awards</h2>
+                <ul style="text-align:left; font-size:1.3em;" >
+                    <li>
+                        1998, 1999, 2001-2004. University of Florida.<br/>
+                        Outstanding International Student (<a href="./images/uf1.jpg" target="_blank">Certificate</a>)
+                    </li>
+                    <li>
+                        2002. Agricultural Development Foundation, Managua, Nicaragua.<br/>
+                        Outstanding Contributions to Farming Systems in Nicaragua (<a href="./images/funica.jpg" target="_blank">Certificate</a>)
+                    </li>
+                    <li>
+                        1993. La Molina Agrarian University, Lima, Peru.<br/>
+                        Honor Diploma: Ranking 2/177 (<a href="./images/unalm1.jpg" target="_blank">Diploma</a>)
+                    </li>
+                </ul>
+            </div>
+
+        </div>
+
+      </div> <!--Row Ends Here-->
+    </div>
+
+    <section class="titleSection2" >
+    	<div class="container">
+    		<div class="row">
+    			<div class="wow animated fadeInUp" data-wow-duration="1s" data-wow-delay="1s">
+    				<h1 class="arrow">Second title</h1><hr>
+    			</div>
+    		</div>
+        	</div>
+        </section>
+
+
+        <div style="background-color: #fff;"  class="container team">
+          <div style="color:black;" class="row">
+            <div class="col-sm-12" style="margin-top: 70px;">
+                <div class="col-sm-12">
+                    <h2>Sample Text</h2>
+                    <ul style="text-align:left; font-size:1.3em;" >
+                        <li>
+                          <p></p>
+                            2016. University of Wisconsin-Madison. Vilas Faculty Mid-Career Investigator Award (<a href="./awards/Cabrera_Victor_Mid_Career_CALS.pdf" target="_blank">Letter</a>) (<a href="http://ecals.cals.wisc.edu/2016/12/29/victor-cabrera-receives-vilas-faculty-mid-career-investigator-award/" target="_blank">News</a>)
+                        </li>
+
+                        <li>
+                            2013. Wisconsin Association of County Agricultural Agents.<br/>
+                            Second Mile Award (<a href="./images/awards/PlaqueSecondMile.png" target="_blank">Plaque</a>) (<a href="./awards/CitationSecondMile.pdf" target="_blank">Citation</a>)
+                        </li>
+                        <li>
+                            2012. University of Wisconsin-Madison College of Agricultural and Life Sciences.<br/>
+                            Pound Extension Award (<a href="./awards/Pound_Letter.pdf" target="_blank">Letter</a>) (<a href="./images/awards/Pound_Plaque.png" target="_blank">Plaque</a>) (<a href="./awards/Pound_Words.pdf" target="_blank">Words</a>)
+                        </li>
+                        <li>
+                            2012. University of Florida School of Natural Resources and Environment.<br/>
+                            Distinguished Achievement Award (<a href="./images/awards/Distinguished_Plaque.png" target="_blank">Plaque</a>) (<a href="./awards/Distinguished_Words.pdf" target="_blank">Words</a>)
+                        </li>
+                        <li>
+                            2011. American Dairy Science Association.<br/>
+                            Foundation Scholar Award in Dairy Production (<a href="./awards/Foundation_Letter.pdf" target="_blank">Letter</a>) (<a href="./images/awards/Foundation_Plaque.png" target="_blank">Plaque</a>) (<a href="./awards/Foundation_Citation.pdf" target="_blank">Citations</a>)
+                        </li>
+                        <li>
+                            2011. University of Wisconsin-Madison College of Agricultural and Life Sciences.<br>
+                            Alfred Toepfer Faculty Fellow Award. (<a href="./awards/Toepfer_Letter.pdf" target="_blank">Letter</a>)
+                        </li>
+                    </ul>
+                </div>
+                </div>
+                </div>
+                </div>
+
+	</div>
+
+
+
+
+
+<!-- /Logo row -->
+				<?php
+				include 'includes/logoRow.php';
+				?>
+<!-- /logo row -->
+
+
+
+<!--FOOTER & scripts-->
+<?php
+include 'includes/footer.php';
+?>
+<!-- /FOOTER scripts-->
+</body>
+</html>
